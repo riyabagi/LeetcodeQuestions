1f4d130a2411fcc3c942001909d7918faaa71d40
Riya Bagi <riyabagi85@gmail.com>
2024-08-08 10:00:47 +0530

    IntersectionOfTwoLinkedList2

--- IntersectonOfTwoLinkedList.cpp
+++ IntersectonOfTwoLinkedList.cpp
@@ -1,3 +1,4 @@
+//Time complexity O(n*M)
 class Solution {
 public:
 	ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
@@ -14,4 +15,36 @@ public:
 		}
 		return NULL;
 	}
+};
+
+//Time complexity O(n + M)
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        if(!headA || !headB) return NULL;
+
+        ListNode * temp1 = headA;
+        ListNode * temp2 = headB;
+
+        while(temp1 != temp2)
+        {
+            if(temp1 == NULL)
+             temp1 = headB;
+            else temp1 = temp1->next;
+            if(temp2 == NULL)
+             temp2 = headA;
+            else temp2 = temp2->next;
+        }
+
+        return temp1;
+    }
 };
